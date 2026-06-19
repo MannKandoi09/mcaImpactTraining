394f1a699317831e8c4f234e6bead35e44b47233
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-19 09:16:56 +0530

    Class Exercise 3 added

---
+++ src/Day11/ClassExercise_3.java
@@ -0,0 +1,24 @@
+package Day11;
+
+// You are using Java
+import java.util.Scanner;
+public class ClassExercise_3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        int count = 1;
+
+        for(int i = 0; i < str.length(); i++){
+            if(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }else{
+                System.out.print(str.charAt(i));
+                System.out.print(count);
+                count = 1;
+            }
+        }
+
+
+    }
+}
