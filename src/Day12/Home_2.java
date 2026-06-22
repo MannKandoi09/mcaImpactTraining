3459b63bd6d3599bfe59e82ec58d325bb8a2c292
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-22 09:26:34 +0530

    Home que 2 added

---
+++ src/Day12/Home_2.java
@@ -0,0 +1,13 @@
+package Day12;
+
+
+import java.util.*;
+public class Home_2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.print(sb.reverse());
+    }
+}
