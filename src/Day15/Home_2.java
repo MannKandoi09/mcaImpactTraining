574329cb16c2f8a5fd8c2db6a5bd2dc332117cac
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-25 13:06:30 +0530

    Home Challange 2 added

---
+++ src/Day15/Home_2.java
@@ -0,0 +1,14 @@
+package Day15;
+/*
++------------------+                 ENROLLS                 +------------------+
+        |     STUDENT      |=========================================|      COURSE      |
+        +------------------+                                         +------------------+
+        | PK Stud_Id       |                                         | PK Course_Id     |
+        |    Stud_Name     |                                         |    Course_Name   |
+        |    Stud_Age      |                                         |                  |
+        +------------------+                                         +------------------+
+
+        (A Student can enroll in many Courses)
+        (A Course can have many Students)
+
+ */
