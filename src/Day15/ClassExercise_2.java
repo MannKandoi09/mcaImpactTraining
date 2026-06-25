395a94e7e6420b69819ca6936a902b8c11eb751f
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-25 08:33:46 +0530

    Class Exercise Que_2 addded

---
+++ src/Day15/ClassExercise_2.java
@@ -0,0 +1,29 @@
+package Day15;
+
+/*
+INSERT INTO Restaurants values
+        (1, "Pasta Paradise", "New York", "Italian"),
+(2, "Sushi World", "Tokyo", "Japanese"),
+        (3, "Taco Heaven", "Los Angeles", "Mexican"),
+        (4, "Curry Corner", "London", "Indian"),
+        (5, "Burger Hub", "New York", "American"),
+        (6, "Pasta Express", "Chicago", "Italian");
+
+
+INSERT INTO Orders values
+        (1,1,101,'2024-07-15',45),
+(2,2,102,'2024-06-20',55),
+        (5,5,105,'2024-06-30',25),
+        (6,6,106,'2024-05-15',60),
+        (8,901,108,'2024-07-01',20);
+
+UPDATE Restaurants
+set Location = 'Rome'
+where CuisineType = 'Italian'
+AND Location <> 'Rome';
+
+DELETE FROM Orders
+WHERE OrderDate < '2023-12-01'
+AND Amount > 50
+OR RestaurantID = 901;
+*/
