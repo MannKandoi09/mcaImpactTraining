158b0274182d22c79119cf76ddf9448153971f02
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-25 12:57:49 +0530

    Home Challange que_1 added

---
+++ src/Day15/Home_1.java
@@ -0,0 +1,14 @@
+package Day15;
+/*
+INSERT INTO BEAUTY_PRODUCTS values
+        ('B001', 'Facial Cleanser', 'Neutrogena', 15.00, 'Skincare', 50),
+('B002', 'Shampoo', 'Pantene', 10.00, 'Haircare', 100),
+        ('B003', 'Lipstick', 'Maybelline', 20.00, 'Makeup', 70),
+        ('B004', 'Body Lotion', 'Nivea', 12.00, 'Skincare', 80),
+        ('B005', 'Perfume', 'Dior', 50.00, 'Fragrance', 60);
+
+UPDATE BEAUTY_PRODUCTS
+SET STOCK = STOCK - 5
+WHERE CATEGORY = 'Skincare';
+
+ */
