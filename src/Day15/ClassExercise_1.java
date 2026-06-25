ac107b4acbbf42f141f5a6c58daf2235b625f6e1
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-25 08:02:23 +0530

    Class Exercise Que_1 added

---
+++ src/Day15/ClassExercise_1.java
@@ -0,0 +1,29 @@
+package Day15;
+/*
+CREATE TABLE Loans(
+        LoanID INT,
+        BorrowerID INT,
+        Amount decimal(10,2),
+InterestRate decimal(5,2),
+StartDate Date
+);
+
+CREATE TABLE Borrowers(
+        BorrowerID INT,
+        Name VARCHAR(100),
+CreditScore INT,
+ContactInfo VARCHAR(255)
+);
+
+CREATE TABLE Repayments(
+        RepaymentID INT,
+        LoanID INT,
+        Amount DECIMAL(10,2),
+RepaymentDate DATE,
+Status VARCHAR(50)
+);
+DROP TABLE Repayments;
+
+ALTER TABLE Loans
+ADD LoanStatus VARCHAR(50);
+*/
