113794e60049d5053ad80cced85014ec270888a3
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-26 10:52:20 +0530

    Home Challange Que_2

---
+++ src/Day16/Home_2.java
@@ -0,0 +1,15 @@
+package Day16;
+/*
+# You are using MYSQL
+SELECT * FROM ExampleTable;
+
+START TRANSACTION;
+
+INSERT INTO ExampleTable(id, name)
+VALUES (3, 'Alice');
+
+COMMIT;
+
+SELECT * FROM ExampleTable;
+
+ */
