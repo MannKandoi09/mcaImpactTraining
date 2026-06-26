420a4c17bf89d2de30c3cec87469ae4589ece99a
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-26 16:02:21 +0530

    Daily Challange added

---
+++ src/Day16/DailyChallange.java
@@ -0,0 +1,25 @@
+package Day16;
+
+/*
+INSERT INTO SPORTS_EQUIPMENT VALUES
+        ('S001', 'Soccer Ball', 'Nike', 25, 'Balls', 100),
+('S002', 'Basketball', 'Spalding', 30, 'Balls', 50),
+        ('S003', 'Tennis Racket', 'Wilson', 120, 'Rackets', 20),
+        ('S004', 'Baseball Glove', 'Rawlings', 60, 'Gloves', 15),
+        ('S005', 'Golf Club Set', 'Callaway', 400, 'Clubs', 10);
+
+UPDATE SPORTS_EQUIPMENT
+SET STOCK = STOCK + 25
+WHERE CATEGORY = 'Balls';
+
+UPDATE SPORTS_EQUIPMENT
+SET PRICE = 100
+WHERE ITEM_NAME = 'Baseball Glove';
+
+ALTER TABLE SPORTS_EQUIPMENT
+DROP COLUMN BRAND;
+
+ALTER TABLE SPORTS_EQUIPMENT
+CHANGE COLUMN CATEGORY ITEM VARCHAR(30);
+
+ */
