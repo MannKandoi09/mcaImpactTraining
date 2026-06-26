8ed768e00e4de2f4777dced552c55e28864cc862
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-26 09:44:46 +0530

    Class Exercise_2 added

---
+++ src/Day16/ClassExercise_2.java
@@ -0,0 +1,22 @@
+package Day16;
+/*
+CREATE TABLE EmployeeSkills (
+        emp_id INT,
+        emp_name varchar(100),
+emp_mobile bigint,
+emp_skill varchar(100)
+
+);
+
+insert into EmployeeSkills values
+        (1, 'John Tick', 9999957773,'Python'),
+(1, 'John Tick',9999957773,'JavaScript'),
+        (2, 'Darth Trader',8888853337,'HTML'),
+        (2, 'Darth Trader',8888853337,'CSS'),
+        (2, 'Darth Trader',8888853337,'JavaScript'),
+        (3, 'Rony Shark',7777720008,'Java'),
+        (3, 'Rony Shark', 7777720008,'Linux'),
+        (3, 'Rony Shark',7777720008,'C++');
+
+
+ */
