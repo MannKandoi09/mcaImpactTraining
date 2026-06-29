63fba369ae5f0a92a4f4718bda2d3bac795806a0
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-29 10:35:34 +0530

    Missing Number Leetcode

---
+++ src/LeetCode/MissingNumber.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int totalSum = n * (n + 1) / 2;
+        int arraySum = 0;
+
+        for(int num : nums){
+            arraySum += num;
+        }
+
+        return totalSum - arraySum;
+
+    }
+}
