07282f4253d08c5e565a0832747515fb42386705
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-05 17:44:23 +0530

    Sort List Leetcode

--- src/Day19/Home_1.java
+++ src/Day19/Home_1.java
@@ -1,4 +1,44 @@
 package Day19;
+/*
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            prices[i] = sc.nextInt();
+        }
 
-public class Home_1 {
+        quickSort(prices, 0, n - 1);
+        for(int i = 0; i < n; i++){
+            System.out.print(prices[i] + (i < n -1? " " : ""));
+        }
+    }
 }
+static void quickSort(int[] arr, int low, int high){
+    if(low < high){
+        int pivotIndex = partition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+static int partition(int[] arr, int low, int high){
+    int pivot = arr[high];
+    int  i = low - 1;
+    for(int j = low; j < high; j++){
+        if(arr[j] <= pivot){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+}
+
+ */
---
+++ src/LeetCode/sortList.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+ /*
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+
+    }
+    ListNode merge(ListNode a, ListNode b){
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while(a != null && b != null){
+            if(a.val < b.val){
+                cur.next = a;
+                a = a.next;
+
+            }else{
+                cur.next = b;
+                b = b.next;
+
+            }
+            cur = cur.next;
+        }
+        cur.next = (a != null) ? a : b;
+
+        return dummy.next;
+    }
+}
+
+  */
