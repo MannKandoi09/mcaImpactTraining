57b2b7dd4aff939c08237baacdf366e70c47c21b
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-12 07:44:04 +0530

    Manager with atleast 5 ReportsLeetcode

---
+++ src/LeetCode/ManagerWithAtleast5Reports.java
@@ -0,0 +1,15 @@
+package LeetCode;
+/*
+public class ManagerWithAtleast5Reports {
+    # Write your MySQL query statement below
+    select e1.name
+    from Employee e1
+    Join Employee e2
+    on e1.id = e2.managerId
+    group by e1.id, e1.name
+    having count(e2.id) >= 5;
+
+}
+
+
+ */
