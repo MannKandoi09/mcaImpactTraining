e5f996045ac654eaf65484fb96e8f06c8e444e49
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-16 15:39:07 +0530

    Customer who never orders leetcode

---
+++ src/LeetCode/CustomerNeverOrders.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class CustomerNeverOrders {
+    /*
+    # Write your MySQL query statement below
+    select c.name as Customers
+    from Customers c
+    left join Orders o
+    on c.id = o.customerId
+    where o.customerId is Null;
+
+     */
+}
