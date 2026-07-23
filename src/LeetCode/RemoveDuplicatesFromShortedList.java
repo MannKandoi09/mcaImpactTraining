da0a5a911ebad782c7186603019e6342a10a29d2
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-23 16:35:05 +0530

    Remove Duplicates from Sorted List LeetCode

---
+++ src/LeetCode/RemoveDuplicatesFromShortedList.java
@@ -0,0 +1,23 @@
+package LeetCode;
+public class RemoveDuplicatesFromShortedList {
+    class Solution {
+        /*
+        public ClassPrinter.ListNode deleteDuplicates(ListNode head) {
+            ListNode current = head;
+
+            while (current != null && current.next != null) {
+
+                if (current.val == current.next.val) {
+                    current.next = current.next.next;
+
+                } else {
+                    current = current.next;
+                }
+            }
+
+            return head;
+
+         */
+
+    }
+}
