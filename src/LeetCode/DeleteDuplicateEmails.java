0cae9a66f52314583a91e453ced6da9b77e4489b
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-24 17:12:59 +0530

    LeetCode Delete Duplicate Emails

---
+++ src/LeetCode/DeleteDuplicateEmails.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class DeleteDuplicateEmails {
+    /*
+    # Write your MySQL query statement below
+    delete from Person
+    where id  not in(
+            SELECT id
+    FROM(
+            select MIN(id) as id
+    from Person
+    group by email
+    ) t
+
+);
+
+     */
+
+}
