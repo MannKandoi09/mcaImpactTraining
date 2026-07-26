0df5ffb96eef19bd0ed67a067345808e4ae5defd
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-26 13:40:14 +0530

    Class Que_1

--- src/LeetCode/Contains_Duplicate.java
+++ src/LeetCode/Contains_Duplicate.java
@@ -1,4 +1,17 @@
 package LeetCode;
-
+import java.util.*;
 public class Contains_Duplicate {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            HashSet<Integer> set = new HashSet<>();
+
+            for(int num: nums){
+                if(set.contains(num)){
+                    return true;
+                }
+                set.add(num);
+            }
+            return false;
+        }
+    }
 }
