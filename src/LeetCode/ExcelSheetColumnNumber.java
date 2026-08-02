f64ea4f6f8aa79fa11b7b1ede9325fc69cb67877
MannKandoi09 <mannkandoi2005@gmail.com>
2026-08-02 13:36:18 +0530

    Excel Sheet Column Number

---
+++ src/LeetCode/ExcelSheetColumnNumber.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class ExcelSheetColumnNumber {
+    class Solution {
+        public int titleToNumber(String columnTitle) {
+            int result = 0;
+
+            for (int i = 0; i < columnTitle.length(); i++) {
+                result = result * 26 + (columnTitle.charAt(i) - 'A' + 1);
+            }
+
+            return result;
+        }
+    }
+}
