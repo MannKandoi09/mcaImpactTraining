c33d21c2ecfa7188c63dbd0b672a30997b96cd80
MannKandoi09 <mannkandoi2005@gmail.com>
2026-08-17 14:37:48 +0530

    Leetcode Total time spend by each Employee

---
+++ src/LeetCode/TotalTimeSpendByEachEmployee.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class TotalTimeSpendByEachEmployee {
+/*    SELECT
+    event_day AS day,
+    emp_id,
+    SUM(out_time - in_time) AS total_time
+    FROM Employees
+    GROUP BY event_day, emp_id;
+
+ */
+}
